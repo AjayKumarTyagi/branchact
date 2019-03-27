c1639d8858a9f3fbd8c0456c29e7214e268999ff
Ajay Tyagi <ajaytyagi0209@gmail.com>
2019-03-27 12:22:56 +0530

    functions called multiple times

--- stack.cpp
+++ stack.cpp
@@ -4,14 +4,19 @@ class stack
 {
 	int stck[9999],top=-1;
 	public:
-	void push();
+	void push(int val);
 	void pop();
 	void display();
 };
 int main()
 {
 	stack s;
-	s.push();
+	s.push(45);
+	s.push(86);
+	s.push(27);
+	s.display();
+	s.pop();
+	s.display();
 	s.pop();
 	s.display();
 }
